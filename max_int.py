5f5062b969ba70db9b1313e34900fa07495b1cd7
Sigurður Jakobsson <iceman13_8@hotmail.com>
2019-09-05 14:11:22 +0000

    Final code written for max_int

--- max_int.py
+++ max_int.py
@@ -1,6 +1,7 @@
 ## Verkefni 05.09.2019
 #Design an algorithm that finds the maximum 
-#positive integer input by a user.  The user repeatedly inputs numbers until a negative value is entered.
+#positive integer input by a user.  The user 
+#repeatedly inputs numbers until a negative value is entered.
 
 #Algorithm:
 #Take input number and put it in variable
@@ -10,6 +11,17 @@
 #Put in greatest number variable if greater than greatest number variable and ask for new number 
 
 num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
-print("The maximum is", max_int)    # Do not change this line
 
+max_int = num_int
+
+if num_int >= 0:
+    
+    while num_int >= 0:
+        
+        num_int = int(input("Input a number: "))
+        
+        if num_int > max_int:
+            max_int = num_int
+    
+    else:
+        print("The maximum is", max_int)    # Do not change this line
