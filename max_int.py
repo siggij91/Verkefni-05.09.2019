2c5249d7d49a4a06d880fca407348fca46d631df
Sigurður Jakobsson <iceman13_8@hotmail.com>
2019-09-05 14:02:17 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,15 @@
+## Verkefni 05.09.2019
+#Design an algorithm that finds the maximum 
+#positive integer input by a user.  The user repeatedly inputs numbers until a negative value is entered.
+
+#Algorithm:
+#Take input number and put it in variable
+#End program if negative
+#Go to while loop if positive and ask for new number
+#If negative end program and print
+#Put in greatest number variable if greater than greatest number variable and ask for new number 
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
+
