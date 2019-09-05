99c4325b96cc34ed9e73bac10f46f86a4538a239
Sigurður Jakobsson <iceman13_8@hotmail.com>
2019-09-05 14:56:12 +0000

    Final code written for sequence

--- sequence.py
+++ sequence.py
@@ -9,3 +9,27 @@
 #repeat until at n
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+nx0 = int()
+nx_min1 = int()
+nx_min2 = int()
+nx_min3 = int()
+
+for num in range(1,n+1):
+    if num == 1:
+        print('1')
+        nx_min3 = 1
+    elif num == 2:
+        print('2')
+        nx_min2 = 2
+    elif num == 3:
+        print('3')
+        nx_min1 = 3
+    else:
+        nx0 = nx_min1 + nx_min2 + nx_min3
+        print(str(nx0))
+        nx_min3 = nx_min2
+        nx_min2 = nx_min1
+        nx_min1 = nx0
+
+#Implementation changed from origininal algorithm
